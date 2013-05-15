84685b1da8ed9d2dfcc3e8d01dcbbf226e88bed5
Sean Walbran <swalbran@socialcast.com>
2013-05-15 11:59:15 -0500

    allow usage with rails 4+

--- lib/jshint.rb
+++ lib/jshint.rb
@@ -2,6 +2,6 @@ require 'jshint/errors'
 require 'jshint/utils'
 require 'jshint/lint'
 
-if defined?(Rails) && Rails::VERSION::MAJOR == 3
+if defined?(Rails) && Rails::VERSION::MAJOR >= 3
   require 'jshint/railtie'
 end
